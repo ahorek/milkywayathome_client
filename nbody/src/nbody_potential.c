846428221a28b742c5db94947cefcab13ab0dbc8
Pavel Rosický <pavel.rosicky@easy.cz>
2026-02-19 02:01:59 +0100

    x

--- nbody/src/nbody_potential.c
+++ nbody/src/nbody_potential.c
@@ -452,7 +452,7 @@ static inline mwvector orbitingBarAccel(const Disk* disk, mwvector pos, real r, 
     real curAngle = (disk->patternSpeed * time * -1)+disk->startAngle;
     //first rotate pos curAngle * -1 radians to emulate the current angle of the bar
     real Radi = mw_sqrt(pos.x*pos.x+pos.y*pos.y);
-    real Phi = mw_atan2(pos.y/pos.x);
+    real Phi = mw_atan(pos.y/pos.x);
     Phi -= curAngle;
     if(pos.x < 0){
         Radi = Radi * -1;
