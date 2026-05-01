adf7fe1d7b4181fa928945f3d8a9959a110d5f07
Pavel Rosický <pavel.rosicky@easy.cz>
2026-05-01 14:26:35 +0200

    -

--- nbody/src/nbody_grav.c
+++ nbody/src/nbody_grav.c
@@ -157,8 +157,7 @@ static inline void nbMapForceBody(const NBodyCtx* ctx, NBodyState* st)
     }
 
   #ifdef _OPENMP
-    //#pragma omp parallel for private(i, b, a, externAcc) shared(bodies, accels) schedule(dynamic, 4096 / sizeof(accels[0]))
-    #pragma omp parallel for private(i, b, a, acc, externAcc) shared(bodies, accels) schedule(dynamic, 4096 / sizeof(accels[0]))    
+    #pragma omp parallel for private(i, b, a, externAcc) shared(bodies, accels) schedule(dynamic, 4096 / sizeof(accels[0]))
   #endif
     for (i = 0; i < nbody; ++i)      /* get force on each body */
     {
@@ -265,8 +264,7 @@ static inline void nbMapForceBody_Exact(const NBodyCtx* ctx, NBodyState* st)
     }
 
   #ifdef _OPENMP
-    //#pragma omp parallel for private(i, b, a, externAcc) shared(bodies, accels) schedule(dynamic, 4096 / sizeof(accels[0]))
-    #pragma omp parallel for private(i, b, a, acc, externAcc) shared(bodies, accels) schedule(dynamic, 4096 / sizeof(accels[0]))     
+    #pragma omp parallel for private(i, b, a, externAcc) shared(bodies, accels) schedule(dynamic, 4096 / sizeof(accels[0]))
   #endif
 
     for (i = 0; i < nbody; ++i)      /* get force on each body */
