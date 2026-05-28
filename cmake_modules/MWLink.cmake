eab6e0c4c90eec99c92737495950b7c198c82802
Kyle Gimmy <kmgimmy413@gmail.com>
2026-05-28 14:42:47 -0400

    Fixed segfault in softening length test

--- cmake_modules/MWLink.cmake
+++ cmake_modules/MWLink.cmake
@@ -47,13 +47,13 @@ function(milkyway_link client_bin_name use_boinc use_static link_libs)
     set(strip_exe "-s")
   endif()
 
-  #if(use_static AND UNIX AND NOT APPLE)
-  #  set(link_flags "-static ${link_flags}")
-  #endif()
-  #
-  #if(use_static AND WIN32 AND MINGW)
-  #  set(link_flags "-static ${link_flags}")
-  #endif()
+  if(use_static AND UNIX AND NOT APPLE)
+    set(link_flags "-static ${link_flags}")
+  endif()
+  
+  if(use_static AND WIN32 AND MINGW)
+    set(link_flags "-static ${link_flags}")
+  endif()
 
   if(HAVE_FLAG_STATIC_LIBSTDCPP)
     set(link_flags "${link_flags} -static-libstdc++ ")
--- nbody/tests/softening_length_test.c
+++ nbody/tests/softening_length_test.c
@@ -27,8 +27,10 @@ int main() {
     real* eps_array;
     unsigned int lm_nbody = 10000;
     unsigned int nbody = 40000;
-    Dwarf* light_comp;
-    Dwarf* dark_comp;
+    Dwarf* light_comp = mwMalloc(sizeof(Dwarf));
+    *light_comp = (Dwarf)EMPTY_DWARF;
+    Dwarf* dark_comp = mwMalloc(sizeof(Dwarf));
+    *dark_comp = (Dwarf)EMPTY_DWARF;
     int failed = 0;
 
     light_comp->mass = 12;
@@ -37,9 +39,9 @@ int main() {
     dark_comp->scaleLength = 0.8;
 
     //Plummer-Plummer
-    eps_l = 0.00000810512565372;
-    eps_cross = 0.00000810512565372;
-    eps_d = 0.00000239956397623;
+    eps_l = 0.00000209086716398582624413214625658241629935218952596187591552734375;
+    eps_cross = 0.00000247278851242636896762684510509000546107927220873534679412841796875;
+    eps_d = 0.00000685814317670222965375075377392199982296006055548787117004394531250;
 
     eps[0] = eps_l;
     eps[1] = eps_cross;
@@ -56,11 +58,14 @@ int main() {
         mw_printf("Got: %.80f, %.80f, %.80f\n", eps_array[0], eps_array[1], eps_array[2]);
         failed = 1;
     }
+    else {
+        mw_printf("Plummer-Plummer model passed\n");
+    }
 
     //Plummer-Hernquist
-    eps_l = 0.0000108469691349;
-    eps_cross = 0.0000108469691349;
-    eps_d = 0.00000640401178546;
+    eps_l = 0.000001847711702106992331048093518297559256780004943720996379852294921875;
+    eps_cross = 0.0000051708933699171807162422477566199319198858574964106082916259765625;
+    eps_d = 0.00000831266825054630985390551056735120027951779775321483612060546875;
 
     eps[0] = eps_l;
     eps[1] = eps_cross;
@@ -76,11 +81,14 @@ int main() {
         mw_printf("Got: %.80f, %.80f, %.80f\n", eps_array[0], eps_array[1], eps_array[2]);
         failed = 1;
     }
+    else {
+        mw_printf("Plummer-Hernquist model passed\n");
+    }
 
     //Plummer-NFW
-    eps_l = 0.0;
-    eps_cross = 0.0;
-    eps_d = 0.0;
+    eps_l = 0.00000204024281439374797800473219921979506352727185003459453582763671875;
+    eps_cross = 0.000003070486122463703874789497927366710428032092750072479248046875;
+    eps_d = 0.00000607115034132603119810915603959955433310824446380138397216796875;
 
     eps[0] = eps_l;
     eps[1] = eps_cross;
@@ -96,11 +104,14 @@ int main() {
         mw_printf("Got: %.80f, %.80f, %.80f\n", eps_array[0], eps_array[1], eps_array[2]);
         failed = 1;
     }
+    else {
+        mw_printf("Plummer-NFW model passed\n");
+    }
 
     //Plummer-Cored
-    eps_l = 0.0;
-    eps_cross = 0.0;
-    eps_d = 0.0;
+    eps_l = 0.000002281581053752841276613221033198186660229112021625041961669921875;
+    eps_cross = 0.000008748023194028815212146266144799255926045589148998260498046875;
+    eps_d = 0.000035177338064575728606396542996748166842735372483730316162109375;
 
     eps[0] = eps_l;
     eps[1] = eps_cross;
@@ -118,11 +129,14 @@ int main() {
         mw_printf("Got: %.80f, %.80f, %.80f\n", eps_array[0], eps_array[1], eps_array[2]);
         failed = 1;
     }
+    else {
+        mw_printf("Plummer-cored model passed\n");
+    }
 
     //Plummer-Cutoff-Cored
-    eps_l = 0.0;
-    eps_cross = 0.0;
-    eps_d = 0.0;
+    eps_l = 0.000002281581053752841276613221033198186660229112021625041961669921875;
+    eps_cross = 0.000008748023194028815212146266144799255926045589148998260498046875;
+    eps_d = 0.000035177338064575728606396542996748166842735372483730316162109375;
 
     eps[0] = eps_l;
     eps[1] = eps_cross;
@@ -138,11 +152,14 @@ int main() {
         mw_printf("Got: %.80f, %.80f, %.80f\n", eps_array[0], eps_array[1], eps_array[2]);
         failed = 1;
     }
+    else {
+        mw_printf("Plummer-cutoff-cored model passed\n");
+    }
 
     //Plummer-Cutoff-NFW
-    eps_l = 0.0;
-    eps_cross = 0.0;
-    eps_d = 0.0;
+    eps_l = 0.00000204024281439380557624514549164285170945731806568801403045654296875;
+    eps_cross = 0.000003070486122463703874789497927366710428032092750072479248046875;
+    eps_d = 0.00000607115034132603119810915603959955433310824446380138397216796875;
 
     eps[0] = eps_l;
     eps[1] = eps_cross;
@@ -159,6 +176,9 @@ int main() {
         mw_printf("Expected: %.80f, %.80f, %.80f\n", eps[0], eps[1], eps[2]);
         mw_printf("Got: %.80f, %.80f, %.80f\n", eps_array[0], eps_array[1], eps_array[2]);
         failed = 1;
+    }
+    else {
+        mw_printf("Plummer-Cutoff-NFW model passed\n");
     }
 
     return failed;
